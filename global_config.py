a754721a7053d218b3d36893c2477fc39589f858
Bohdan Boklah <b.boklag@gmail.com>
2025-08-22 01:02:59 +0300

    add DEVICE

--- global_config.py
+++ global_config.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+import torch
 
 BASE_PATH = Path('/kaggle/input/airbus-ship-detection')
 TRAIN_LABEL_PATH = BASE_PATH / 'train_ship_segmentations_v2.csv'
 TEST_LABEL_PATH = BASE_PATH / 'sample_submission_v2.csv'
 TRAIN_SAMPLE_DIR = BASE_PATH / 'train_v2'
 TEST_SAMPLE_DIR = BASE_PATH / 'test_v2'
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+CLASSES = 1
