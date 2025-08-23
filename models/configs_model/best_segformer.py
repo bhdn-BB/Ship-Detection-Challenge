abae324912455240492af9cff951f0d6080ff18a
Bohdan Boklah <b.boklag@gmail.com>
2025-08-23 10:27:16 +0300

    add config for segformer

---
+++ models/configs_model/best_segformer.py
@@ -0,0 +1,13 @@
+
+MODEL_NAME = "nvidia/segformer-b5-finetuned-cityscapes-1024-1024"
+CLASSES = 2
+
+OUTPUT_SEGFORMER = '/kaggle/working/best_segformer.pth'
+
+PATIENCE = 10
+LOG_STEPS = 10
+WEIGHT_DECAY = 1e-4
+
+EPOCHS = 50
+BATCH_SIZE = 128
+LR = 3e-4
