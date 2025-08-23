d63abef4793b5e27eee789f0d8b893af674fa60f
Bohdan Boklah <b.boklag@gmail.com>
2025-08-23 10:26:53 +0300

    fix augmentation

--- processing/augmentation.py
+++ processing/augmentation.py
@@ -1,35 +1,28 @@
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-def train_transform(height: int, width: int):
-    return A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.VerticalFlip(p=0.5),
-        A.RandomRotate90(p=0.5),
-        A.ShiftScaleRotate(
-            shift_limit=0.05,
-            scale_limit=0.05,
-            rotate_limit=10,
-            border_mode=0,
-            p=0.3
-        ),
-        A.RandomBrightnessContrast(p=0.5),
-        A.ColorJitter(
-            brightness=0.2,
-            contrast=0.2,
-            saturation=0.1,
-            hue=0.05,
-            p=0.3
-        ),
-        A.GaussianBlur(blur_limit=(3,5), p=0.2),
-        A.Resize(height=height, width=width),
-        A.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5)),
-        ToTensorV2(),
-    ])
+from global_config import HEIGHT, WIDTH
 
-def val_transform(height: int, width: int):
-    return A.Compose([
-        A.Resize(height=height, width=width),
-        A.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5)),
-        ToTensorV2(),
-    ])
+train_transform = A.Compose([
+    A.Resize(height=HEIGHT, width=WIDTH),
+    A.HorizontalFlip(p=0.5),
+    A.VerticalFlip(p=0.5),
+    A.RandomRotate90(p=0.5),
+    A.ShiftScaleRotate(
+        shift_limit=0.05,
+        scale_limit=0.05,
+        rotate_limit=15,
+        border_mode=0,
+        p=0.5
+    ),
+    A.Normalize(mean=(0.485, 0.456, 0.406),
+                std=(0.229, 0.224, 0.225)),
+    ToTensorV2(),
+])
+
+val_transform = A.Compose([
+    A.Resize(height=HEIGHT, width=WIDTH),
+    A.Normalize(mean=(0.485, 0.456, 0.406),
+                std=(0.229, 0.224, 0.225)),
+    ToTensorV2(),
+])
